4164ee298ece5be40e74d654bd4d83c7d01342f9
Shawn Ma <shawnma@google.com>
2023-03-05 07:37:46 -0800

    Update script

--- asy.sh
+++ asy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -ex
 pdflatex -synctex=1 -interaction=nonstopmode -file-line-error $1
-asy *.asy
-pdflatex -synctex=1 -interaction=nonstopmode -file-line-error $1
+cnt=0
+for f in *.asy; do
+  sum=$(sum $f|awk '{print $1}')
+  sumfile="${f}.sum"
+  [ -f $sumfile ] && old=$(cat $f.sum)
+  if [ "$sum" != "$old" ]; then
+    asy $f
+    echo $sum > $sumfile
+    cnt=$(expr $cnt + 1)
+  fi
+done
+if [ $cnt -gt 0 ]; then
+  pdflatex -synctex=1 -interaction=nonstopmode -file-line-error $1
+fi
