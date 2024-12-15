a5a044ac49d153cad55cb1a0a922694e9a52dc61
Shawn Ma <shawnma@gmail.com>
2024-12-14 16:21:22 -0800

    Add 3 methods to be implemented for Piece.java

--- apcsa/tetris/piece/Piece.java
+++ apcsa/tetris/piece/Piece.java
@@ -1,10 +1,12 @@
+
 import java.awt.*;
 
 class Point {
+
     final int x;
     final int y;
 
-    private Point(int x, int y) {
+    Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -15,25 +17,88 @@ class Point {
 }
 
 public class Piece {
+
+    private Color color;
+    private Point[] points;
+    private Piece next;
+
+    private Piece(Color color, int[] coords) {
+        this.color = color;
+        points = new Point[4];
+        for (int i = 0; i < 4; i++) {
+            points[i] = Point.of(coords[i * 2], coords[i * 2 + 1]);
+        }
+    }
+
+    private Piece(Color color, Point[] points) {
+        this.color = color;
+        this.points = points;
+    }
+
     public static Piece[] getPieces() {
-        return new Piece[]{new Piece()};
+        Piece l = constructRow(Color.MAGENTA, new int[]{0, 0, 0, 1, 0, 2, 1, 0}, 4);
+        Piece l2 = constructRow(Color.RED, new int[]{0, 0, 1, 1, 1, 0, 1, 2}, 4);
+        Piece dog = constructRow(Color.ORANGE, new int[]{0, 0, 1, 0, 1, 1, 2, 1}, 2);
+        Piece dog2 = constructRow(Color.YELLOW, new int[]{0, 1, 1, 1, 1, 0, 2, 0}, 2);
+        Piece line = constructRow(Color.CYAN, new int[]{0, 0, 0, 1, 0, 2, 0, 3}, 2);
+        Piece t = constructRow(Color.GREEN, new int[]{0, 0, 1, 0, 2, 0, 1, 1}, 4);
+        Piece square = constructRow(Color.BLUE, new int[]{0, 0, 0, 1, 1, 0, 1, 1}, 1);
+        return new Piece[]{l, l2, dog, dog2, line, t, square};
+    }
+
+    private static Piece constructRow(Color color, int[] coords, int n) {
+        Piece root = new Piece(color, coords);
+        Piece current = root;
+        for (int i = 0; i < n-1; i++) {
+            Piece next = current.rotate();
+            current.next = next;
+            current = next;
+        }
+        current.next = root;
+        return root;
     }
 
     public Color getColor() {
-        return Color.RED;
+        return color;
     }
 
-    public Piece rotate() {
-        return this;
+    public Piece next() {
+        return next;
+    }
+
+    private Piece rotate() {
+        Point[] p = new Point[4];
+        int shift = 0;
+        for (int i = 0; i < 4; i++) {
+            if (points[i].y > shift) {
+                shift = points[i].y;
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            Point old = points[i];
+            int newx = -old.y + shift;
+            int newy = old.x;
+            p[i] = Point.of(newx, newy);
+        }
+        return new Piece(color, p);
     }
 
     public Point[] getPoints() {
-        Point p[] = new Point[16];
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                p[i * 4 + j] = Point.of(i, j);
-            }
-        }
-        return p;
+        return points;
+    }
+
+    public int getWidth() {
+        // TODO
+        return 0;
+    }
+
+    public int getHeight() {
+        // TODO
+        return 0;
+    }
+
+    public int[] getSkirt() {
+        // TODO
+        return new int[3];
     }
 }
--- apcsa/tetris/piece/PieceTest.java
+++ apcsa/tetris/piece/PieceTest.java
@@ -1,17 +1,19 @@
-import javax.swing.*;
+
 import java.awt.*;
+import java.util.Arrays;
+import javax.swing.*;
 
 public class PieceTest extends JComponent {
+
     private final Piece root;
 
     public PieceTest(Piece piece, int width, int height) {
         super();
-
         setPreferredSize(new Dimension(width, height));
-
         root = piece;
     }
 
+    @Override
     public void paintComponent(Graphics g) {
         int MAX_ROTATIONS = 4;
         int padding = 4;
@@ -19,43 +21,44 @@ public class PieceTest extends JComponent {
         Piece p = root;
         for (int i = 0; i < MAX_ROTATIONS; i++) {
             drawPiece(g, p, new Rectangle(i * size + padding, padding, size, size));
-            p = p.rotate();
+            p = p.next();
             if (p.equals(root)) {
-                //break;
+                break;
             }
         }
-     }
+    }
 
     /**
      * Draw the piece inside the given rectangle.
      */
     private void drawPiece(Graphics g, Piece piece, Rectangle r) {
         g.setColor(piece.getColor());
-        //g.clipRect(r.x,r.y,r.width,);
-        //g.drawRect(r.x, r.y, r.width, r.height);
         int padding = 2;
-        int size = r.width/4-padding;
+        int size = r.width / 4 - padding;
         for (Point p : piece.getPoints()) {
-            g.fillRect(r.x+p.x*size+padding, r.y+(3-p.y)*size+padding, size-padding,size-padding);
+            g.fillRect(r.x + p.x * size + padding, r.y + (3 - p.y) * size + padding, size - padding, size - padding);
         }
+        g.setColor(Color.BLUE);
+        g.setFont(Font.getFont("sans"));
+        g.drawString(String.format("(%d,%d): %s",
+                piece.getWidth(), piece.getHeight(), Arrays.toString(piece.getSkirt())), r.x, r.height + padding);
     }
 
-
     /**
-     * Draws all the pieces by creating a JPieceTest for
-     * each piece, and putting them all in a frame.
+     * Draws all the pieces by creating a JPieceTest for each piece, and putting
+     * them all in a frame.
      */
     static public void main(String[] args) {
         JFrame frame = new JFrame("Piece Tester");
         Container container = frame.getContentPane();
 
         // Put in a BoxLayout to make a vertical list
-        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        container.setLayout(new GridLayout(4, 2));
 
         Piece[] pieces = Piece.getPieces();
 
-        for (int i = 0; i < pieces.length; i++) {
-            PieceTest test = new PieceTest(pieces[i], 400, 100);
+        for (Piece piece : pieces) {
+            PieceTest test = new PieceTest(piece, 800, 200);
             container.add(test);
         }
 
